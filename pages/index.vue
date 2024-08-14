be801a944dbfc10ff75a47ec6fa595895a63be2f
Hari Kotha <hkotha@ky.gov>
2024-08-14 17:30:00 -0400

    Added logic to calculate the overall summary of transaction based on date range

--- pages/index.vue
+++ pages/index.vue
@@ -8,8 +8,8 @@
         </div>
     </section>
     <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10">
-      <Trend color="green" title="Income" :amount="incomeTotal" :last-amount="4100" :loading="pending" />
-      <Trend color="red" title="Expense" :amount="expenseTotal" :last-amount="3800" :loading="pending" />
+      <Trend color="green" title="Income" :amount="incomeTotal" :last-amount="prevIncomeTotal" :loading="pending" />
+      <Trend color="red" title="Expense" :amount="expenseTotal" :last-amount="prevExpenseTotal" :loading="pending" />
       <Trend color="green" title="Investments" :amount="4000" :last-amount="3000" :loading="pending" />
       <Trend color="red" title="Saving" :amount="4000" :last-amount="4100" :loading="pending" />
     </section>
@@ -43,6 +43,7 @@
     import { transactionViewOptions } from '~/constants';   
     const selectedView = ref(transactionViewOptions[1])
     const isOpen = ref(false)
+    const { current, previous } = useSelectedTimePeriod(selectedView)
 
     const { pending, refresh, transactions: {
     incomeCount,
@@ -52,6 +53,11 @@
     grouped: {
     byDate
   }
-} } = useFetchTransactions()
-await refresh()
+} } = useFetchTransactions(current)
+
+const { transactions: {
+  incomeTotal: prevIncomeTotal,
+  expenseTotal: prevExpenseTotal,
+} } = useFetchTransactions(previous)
+
 </script>
