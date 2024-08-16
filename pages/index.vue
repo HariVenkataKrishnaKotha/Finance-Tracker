797f5d7c9ff9cf4ecbdb1dd247382d28e2813de9
hvkkotha <hk00016@mix.wvu.edu>
2024-08-16 14:31:53 -0400

    Fixed logic to calculate summary tredns and fetching transactions as per the range selected

--- pages/index.vue
+++ pages/index.vue
@@ -55,9 +55,10 @@
   }
 } } = useFetchTransactions(current)
 
-const { transactions: {
+const { refresh: refreshPrevious, transactions: {
   incomeTotal: prevIncomeTotal,
   expenseTotal: prevExpenseTotal,
 } } = useFetchTransactions(previous)
 
+await Promise.all([refresh(), refreshPrevious()])
 </script>
